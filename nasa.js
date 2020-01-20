da844e8ae561da47850bd16357b4f4a1d7255b30
Austen Haymond <ahaymon17@gmail.com>
2020-01-19 18:11:48 -0700

    added if/else for nasa api, added bio info and styling

--- nasa.js
+++ nasa.js
@@ -6,12 +6,22 @@ $.ajax({
     method: "GET"
   }).then(function(response) {
         $(document).ajaxSuccess(function() {
+          if (response.media_type === "photo") {
             $(".row").css("backgroundImage", "url(" + response.url + ")");
             $(".row").css({height: '700px' });
             $(".row").css({width: "100%"});
+            $(".row").css("position", "absolute");
+            $(".row").css("backgroundRepeat", "repeat");
+            // $(".container").css({height: "auto"}); 
+            console.log(response); 
+          }
+          else {
+            $(".container").css("backgroundImage", "url(milky_way.jpg)");
+            $(".container").css({height: "1000px" });
+            $(".container").css({width: "100%"});
             // $(".row").css("position", "absolute");
-            // $(".row").css("backgroundRepeat", "repeat");
-            // $(".container").css({height: "auto"});   
+            $(".container").css("backgroundRepeat", "repeat");
+          }; 
             
         });
 
