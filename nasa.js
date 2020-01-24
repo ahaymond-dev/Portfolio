d5d67b3fd1128edf22bf71e41451a430e40116f0
Austen Haymond <ahaymon17@gmail.com>
2020-01-23 18:35:34 -0700

    fixed js for nasa api call

--- nasa.js
+++ nasa.js
@@ -6,14 +6,17 @@ $.ajax({
     method: "GET"
   }).then(function(response) {
         $(document).ajaxSuccess(function() {
-          if (response.media_type === "photo") {
-            $(".row").css("backgroundImage", "url(" + response.url + ")");
-            $(".row").css({height: '700px' });
-            $(".row").css({width: "100%"});
-            $(".row").css("position", "absolute");
-            $(".row").css("backgroundRepeat", "repeat");
-            // $(".container").css({height: "auto"}); 
-            console.log(response); 
+          console.log(response);
+          if (response.media_type === "image") {
+            $(".container").css("backgroundImage", "url(" + response.url + ")");
+            $(".container").css({height: '100%' });
+            $(".container").css({width: "100%"});
+            $(".container").css("position", "absolute");
+            $(".container").css("backgroundRepeat", "repeat");
+            // $(".row").css("backgroundSize", "auto");
+            $(".container").css({height: "auto"}); 
+            $(".container").css("margin", "auto");
+            // console.log(response); 
           }
           else {
             $(".container").css("backgroundImage", "url(milky_way.jpg)");
